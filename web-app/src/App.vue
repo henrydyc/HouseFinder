a9234b0b82e240b51d6f89bdcf58fa3921cf55e4
henrydyc <henrydyc@hotmail.com>
2017-02-27 21:05:24 -0800

    added VueJS skeleton for the web app

---
+++ web-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <app-address-form></app-address-form>
+    <app-charts-carousel></app-charts-carousel>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header.vue';
+  import AddressForm from './components/AddressForm.vue';
+  import ChartsCarousel from './components/ChartsCarousel.vue';
+
+  export default {
+    data: function (){
+      return {
+      }
+    },
+    components: {
+      appHeader: Header,
+      appAddressForm: AddressForm,
+      appChartsCarousel: ChartsCarousel
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ web-app/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <h3>Search Your Property Here</h3>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+
+</style>
